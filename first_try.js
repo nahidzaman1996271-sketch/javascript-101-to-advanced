551fa57e3a0105eb5f45fb805c531b52255fbb0e
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-07-11 00:02:51 +0600

    and here we start learninh the JavaScript and it won't be that easy based on the introduction things i can already told that

---
+++ first_try.js
@@ -0,0 +1,11 @@
+// console.log(`Hello`);
+// console.log(`Hey it's me Nahid Ibn Zaman`);
+
+// window.alert(`This is an alert!`);
+// window.alert(`I like Pizza`);
+
+
+document.getElementById("myH1").textContent = `Hello`;
+document.getElementById("myP").textContent = `I love to do the gym`;
+
+// This is a comment
