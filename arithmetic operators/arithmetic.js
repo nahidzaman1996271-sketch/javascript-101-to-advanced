e8ad17245f81a61817822625a21f319ecb541e4a
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-07-11 22:05:22 +0600

    and this is the arithmetic operators and here is the all code and as you guy's can see this is the all i had to teach you and here we finish the arithmetic operator

---
+++ arithmetic operators/arithmetic.js
@@ -0,0 +1,36 @@
+// arithmetic opertaors = operands (values, variables, etc)
+//                        operators (- / * +)
+//                        ex. 11 = x + 5;
+
+// let students = 30;
+
+// students = students + 1;
+// students = students - 1;
+// students = students * 2;
+// students = students / 2;
+// students = students ** 2;
+// students = students % 2;
+// let extraStudents = students % 3;
+
+// These are the shortcuts of the above funtions
+
+// students += 1;
+// students -= 1;
+// students /= 1;
+// students *= 1;
+// students %= 1;
+
+// students++;
+// students--;
+
+/*
+    operator precedence
+    1. parenthesis ()
+    2. exponents
+    3. multiplication & division & modulo
+    4. addition & substraction
+*/
+
+let result = 1 + 2 * 3 + 4 ** 2;
+
+console.log(result);
