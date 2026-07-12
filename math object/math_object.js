0a37437d738c58855aec7cb6afaccb8f17350501
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-07-13 02:45:54 +0600

    and this is the math objects in javascript and it is pretty familiar to cpp OOP

---
+++ math object/math_object.js
@@ -0,0 +1,23 @@
+// Math = built-in object that provides a
+//        collection of properties and methods
+
+let x = 3.99;
+let y = 2;
+let z = 1;
+
+z = Math.round(x);
+z = Math.floor(x);
+z = Math.ceil(x);
+z = Math.truncate(x);
+z = Math.pow(x,9);
+z = Math.sqrt(x);
+z = Math.log(x);
+z = Math.sin(x);
+z = Math.cos(x);
+z = Math.tan(x);
+z = Math.abs(x);
+z = Math.sign(x);
+let max = Math.max(x,y,z);
+let min = Math.min(x,y,z);
+
+console.log(max);
