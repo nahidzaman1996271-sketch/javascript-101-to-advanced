cf03804654792c00ec9dec2c5d88daa7aa1bbfc7
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-07-17 22:15:29 +0600

    after a long time we back to JavaScript and guy's this is ternary operator and it is good to learn cause less code bigger output

---
+++ Ternary operator/ternary.js
@@ -0,0 +1,17 @@
+
+/* let age = 21;
+let message = age >= 18 ? "You're an adult" : "You're a minor";
+console.log(message);
+
+let time = 16;
+let greeting = time < 12 ? "Good morning!" : "Good afternoon!";
+console.log(greeting);
+
+let isStudent = true;
+let message = isStudent ? "You're a student" : "You are NOT a student!";
+console.log(message);
+
+let purchaseAmount = 125;
+let discount = puchaseAmount > 100 ? 10 : 0;
+console.log(`Your total is $${purchaseAmount - purchaseAmount * (discount/100)}`);
+*/
