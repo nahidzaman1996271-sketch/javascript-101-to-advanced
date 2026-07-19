7ee83ffdd82fcc20ede2f5a2399f10c36ed844af
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-07-19 22:39:18 +0600

    and those are string methods in JavaScript

---
+++ string methods/string_methods.js
@@ -0,0 +1,13 @@
+// string methods = allow you to manipulate and work with the text (strings)
+
+let userName = "Nahid Ibn Zaman";
+
+// userName = userName.toUpperCase();
+
+// console.log(userName);
+
+let phoneNumber = "123-456-789";
+
+phoneNumber = phoneNumber.padStart(15,"0");
+
+console.log(phoneNumber);
