be34d9047c0f0e1dfe15d57f8b5c416add30a0f7
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-07-24 15:36:02 +0600

    and here is the string slicing topic and it was fun and learning and meaningful and we learnt this in the python also

---
+++ string slicing/string_slicing.js
@@ -0,0 +1,15 @@
+// const fullName = "Nahid Zaman";
+
+// let firstName = fullName.slice(0,fullName.indexOf(" "));
+// let lastName = fullName.slice(fullName.indexOf(" ") + 1);
+
+// console.log(firstName);
+// console.log(lastName); 
+
+const email = "Nahid1@gmail.com";
+
+let username = email.slice(0,email.indexOf("@"));
+let extension = email.slice(email.indexOf("@") + 1);
+
+console.log(username);
+console.log(extension);
