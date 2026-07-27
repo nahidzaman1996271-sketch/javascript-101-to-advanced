bd5b61a34b76d6febcc398daae9bf536d246e407
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-07-27 22:22:26 +0600

    and this is the strict equality

---
+++ strict equality/strict_equality.js
@@ -0,0 +1,16 @@
+//      assignment operator
+// ==   comparison operator (compare if values are equal)
+// ===  strict equality operator (compare if values & datatype are equal)
+// !=   inequality operator
+// !=   strict inequality operator
+
+const pi = 3.14;
+
+if(pi === "3.14"){
+    console.log("That is PI");
+}
+else{
+    console.log("That is not PI");
+}
+
+
