22f738a49bedb75236eebe45fcb589ca7dff9408
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-08-06 00:47:21 +0600

    that's the while loops in JavaScript

--- while loops/while_loops.js
+++ while loops/while_loops.js
@@ -1,1 +1,19 @@
 // while loop = repeat some code WHILE some condition is true
+
+let loggedIn = false;
+let username;
+let password;
+
+while(!loggedIn){
+    username = window.prompt(`Enter your username`);
+    password = window.prompt(`Enter your password`);
+
+    if(username === "myUsername" && password === "myPassword"){
+        loggedIn = true;
+        console.log("You're logged in!");
+    }else{
+        console.log("Invalid credentials! Please try again");
+    }
+}
+
+// you can also do this with the do while loops make sure to try that
