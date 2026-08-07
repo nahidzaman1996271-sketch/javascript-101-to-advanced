b375a5b9e05e86ca11a4f035ef1685e4d6f52970
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-08-08 01:17:47 +0600

    and here is the temperature conversion using html, css and farhenheit

---
+++ temperature conversion/tc.js
@@ -0,0 +1,25 @@
+// TEMPERATURE CONVERSION FUNCTION
+
+const textbox = document.getElementById("textbox");
+const toFarhenheit = document.getElementById("toFarhenheit");
+const toCelcius = document.getElementById("toCelcius");
+const result = document.getElementById("result");
+
+function convert(){
+
+    if(toFarhenheit.checked){
+        temp = Number(textbox.value);
+        temp = temp * 9 / 5 + 32;
+        result.textContent = temp.toFixed(1) + "°F";
+    }
+    else if(toCelcius.checked){
+        temp = Number(textbox.value);
+        temp = (temp - 32)*(5/9);
+        result.textContent = temp.toFixed(1) + "°C";
+    }
+    else{
+        result.textContent = "Select a unit";
+    }
+
+}
+
