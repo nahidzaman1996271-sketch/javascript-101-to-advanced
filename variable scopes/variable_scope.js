62ee318c692e8ef48777615097475270f907f550
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-08-08 00:29:21 +0600

    and that is variable scope in JavaScript

---
+++ variable scopes/variable_scope.js
@@ -0,0 +1,15 @@
+// variable scope = where a variable is recognized and accessible (local vs global)
+
+function1();
+
+let x = 3;
+
+function function1(){
+    let x = 1;
+    console.log(x);
+}
+
+function function2(){
+    let x = 2;
+    console.log(x);
+}
