c5a7472650bb8be53c473dc7b64868249810abf0
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-08-14 15:18:14 +0600

    so this is the spread operator

--- spread operator/sp_op.js
+++ spread operator/sp_op.js
@@ -1,2 +1,9 @@
 // spread operator = ... allows an iterable such as an array or string to be
 //                     expanded into separate elements (unpacks the elements)
+
+let fruits = ["apple","orange","banana"];
+let vegetables = ["carrots","celery","potatoes"];
+
+let foods = [...fruits, ...vegetables, "eggs","milk"];
+
+console.log(foods);
