3b46a09ccfd3dcb663893d67296ab2e91441e1f7
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-08-24 21:17:45 +0600

    so this is callbacks in JavaScript

--- callbacks/index.js
+++ callbacks/index.js
@@ -1,8 +1,16 @@
 // callback = a function that is passed as an argument to another function.
 
-// used to handle asynchronous operations:
-// 1. Reading a file
-// 2. Network requests
-// 3. Interacting with databases
+sum(displayPage, 1, 2);
 
-// "Hey, when you're done, call this next."|
+function sum(callback, x, y){
+    let result = x + y;
+    callback(result);
+}
+
+function displayConsole(result){
+    console.log(result);
+}
+
+function displayPage(result){
+    document.getElementById("myH1").textContent = result;
+}
