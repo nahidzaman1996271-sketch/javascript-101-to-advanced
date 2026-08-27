d9e04b5beebb96067d19df7e89a1f01232e0e14e
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-08-27 17:49:03 +0600

    so this is the arrow topics in JavaScipt and this is pretty good to work with

---
+++ arrow functions/index.js
@@ -0,0 +1,13 @@
+// arrow functions = a concise way to write function expressions good for simple functions that you only use once
+
+const numbers = [1,2,3,4,5,6];
+
+const squares = numbers.map((element) => Math.pow(element,2));
+const cubes = numbers.map((element) => Math.pow(element,3));
+const evenNums = numbers.filter((element) => element %2 === 0);
+const oddNums = numbers.filter((element) => element %2 !== 0);
+const total = numbers.reduce((accumulator, element) >= accumulator + element);
+
+console.log(cubes);
+console.log(evenNums);
+console.log(oddNums);
