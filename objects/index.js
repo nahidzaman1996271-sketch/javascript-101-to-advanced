b70367b20eeefbcdc1efec76166ee1dfbef4b035
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-08-28 22:43:05 +0600

    this is it........

---
+++ objects/index.js
@@ -0,0 +1,18 @@
+const person1 = {
+    firstName: "Nahid Ibn",
+    LastName: "Zaman",
+    age: 30,
+    sayHello: function(){console.log("Hi I am Nahid Ibn Zaman")},
+    eat: function(){console.log("I am eating a Tuna Fish")},
+}
+
+const person2 = {
+    firstName: "Robert Downy",
+    LastName: "Jr",
+    age: 70,
+    sayHello: function(){console.log("Hi I am Robert Downy jr")},
+    eat: function(){console.log("I am eating a burger with exrta cheese")},
+}
+
+person1.eat();
+person2.eat();
