39496f91b05f161a156b9cf1834bcb2163e78edf
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-08-31 23:27:02 +0600

    this is nice

---
+++ getters and setters/index.js
@@ -0,0 +1,107 @@
+// getter = special method that makes a property readable
+// setter = special method that makes a property writeable
+
+// validate and modify a value when reading/writing a property
+
+/* below everything is a example
+class Rectangle{
+
+    constructor(width, height){
+        this.width = width;
+        this.height = height;
+    }
+
+    set width(newWidth){
+        if(newWidth > 0){
+            this._width = newWidth;
+        }
+        else{
+            console.error("Width must be a positive number");
+        }
+    }
+
+    set height(newHeight){
+        if(newHeight > 0){
+            this._height = newHeight;
+        }
+        else{
+            console.error("Height must be a positive number");
+        }
+    }
+
+    get width(){
+        return this._width.toFixed(1);
+    }
+
+    get height(){
+        return this._height;
+    }
+
+    get area(){
+        return this._width * this._height;
+    }
+}
+
+const rectangle = new Rectangle(3, 5);
+
+rectangle.width = 4;
+rectangle.height = 20
+
+console.log(rectangle.width);
+console.log(rectangle.height);
+console.log(rectangle.area);
+*/
+
+class Person {
+
+    constructor(firstName, lastName, age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    set firstName(newFirstName) {
+        if (typeof newFirstName === "string" && newFirstName.length > 0) {
+            this._firstName = newFirstName;
+        }
+        else {
+            console.error("First name must be a non-empty string");
+        }
+    }
+
+    set lastName(newLastName) {
+        if (typeof newLastName === "string" && newLastName.length > 0) {
+            this._lastName = newLastName;
+        }
+        else {
+            console.error("Last name must be a non-empty string");
+        }
+    }
+
+    set age(newAge) {
+        if (typeof newAge === "number" && newAge >= 0) {
+            this._age = newAge;
+        }
+        else {
+            console.error("Age must be a non-negative number");
+        }
+    }
+
+    get firstName(){
+        return this._firstName;
+    }
+    
+    get lastName(){
+        return this._lastName;
+    }
+    get age(){
+        return this._age;
+    }
+
+}
+
+const person = new Person("Nahid", "Ibn Zaman", 12);
+
+console.log(person.firstName);
+console.log(person.lastName);
+console.log(person.age);
