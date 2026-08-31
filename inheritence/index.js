8e33ca4515a8dff21f94dedf97d08b6f7dae2687
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-08-31 22:28:12 +0600

    so this is inheritence in JavaScript

--- inheritence/index.js
+++ inheritence/index.js
@@ -1,3 +1,36 @@
 // inheritance = allows a new class to inherit properties and methods
 //               from an existing class (parent -> child)
 //               helps with code reusability
+
+class Animal {
+    alive = true;
+
+    sleep() {
+        console.log(`${this.name} is sleeping...`);
+    }
+}
+
+class Rabbit extends Animal {
+    name = "rabbit";
+}
+
+class Fish extends Animal {
+    name = "fish";
+}
+
+class Hawk extends Animal {
+    name = "hawk";
+}
+
+const rabbit = new Rabbit();
+const fish = new Fish();
+const hawk = new Hawk();
+
+console.log(rabbit.alive);
+rabbit.sleep();
+
+console.log(fish.alive);
+fish.sleep();
+
+console.log(hawk.alive);
+hawk.sleep();
