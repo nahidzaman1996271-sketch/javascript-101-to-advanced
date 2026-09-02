01feae1a181bc55c6543c37614401de80ff112fd
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-09-02 17:08:23 +0600

    those are dates in JavaScript

---
+++ date/index.js
@@ -0,0 +1,9 @@
+// Date objects = Objects that contain values that represent dates and times
+//                These date objects can be changed and formatted
+
+const date1 = new Date("2023-12-31");
+const date2 = new Date("2024-01-01");
+
+if(date2 > date1){
+    console.log("HAPPY NEW YEAR!");
+}
