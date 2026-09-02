69f00e6f2be72b85897b8ba9e66f1f196775e12c
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-09-02 21:22:50 +0600

    this is for today in JavaScript

--- closures/index.js
+++ closures/index.js
@@ -1,5 +1,29 @@
 // closure = A function defined inside of another function,
-// the inner function has access to the variables
-// and scope of the outer function.
-// Allow for private variables and state maintenance
-// Used frequently in JS frameworks: React, Vue, Angular
+//           the inner function has access to the variables
+//           and scope of the outer function.
+//           Allow for private variables and state maintenance
+//           Used frequently in JS frameworks: React, Vue, Angular
+
+function createCounter(){
+
+    let count = 0;
+
+    function increment(){
+        count++;
+        console.log(`Count increased to ${count}`);
+    }
+
+    function getCount(){
+        return count;
+    }
+
+    return {increment, getCount}
+}
+
+const counter = createCounter();
+
+counter.increment();
+counter.increment();
+counter.increment();
+
+console.log(`The current count in ${counter.getCount}`);
