83169a37657638cc37de5b95219f18483c4ae6a0
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-09-02 17:10:49 +0600

    i will start this in the night

---
+++ closures/index.js
@@ -0,0 +1,5 @@
+// closure = A function defined inside of another function,
+// the inner function has access to the variables
+// and scope of the outer function.
+// Allow for private variables and state maintenance
+// Used frequently in JS frameworks: React, Vue, Angular
