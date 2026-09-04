ca3042353ea9c8b56f4fe607e6b610b63897b992
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-09-04 22:19:50 +0600

    i'll start this tomorrow

---
+++ ES6 modules/index.js
@@ -0,0 +1,6 @@
+// ES6 Module = An external file that contains reusable code
+//              that can be imported into other JavaScript files.
+//              Write resusable code for many different apps.
+//              Can contain variables, classes, functions ... and more
+//              Introduced as part of ECMAScript 2015 update
+
