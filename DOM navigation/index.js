dbfde85364285abdad232aab4c2ccdd4445daa5c
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-09-09 22:13:56 +0600

    and this is the DOM navigation in JavaScript and this was awesome and we all love it as far as we can

--- DOM navigation/index.js
+++ DOM navigation/index.js
@@ -9,3 +9,50 @@
 // .children
 
 
+// -- .firstElementChild -- //
+/*
+const ulElement = document.querySelectorAll("ul");
+
+ulElements.forEach(ulElement => {
+    const firstChild = ulElement.firstChild;
+    firstChild.style.backgroundColor = "yellow";
+});
+*/
+
+// -- .lasstElementChild -- //
+/*
+const ulElements = document.querySelectorAll("ul");
+
+ulElements.forEach(ulElement => {
+    const lastChild = ulElement.lastElementChild;
+    lastChild.style.backgroundColor = "yellow";
+});
+*/
+
+// -- .nextElementSibling -- //
+/*
+const element = document.getElementById("apple");
+const nextSibling = element.nextElementSibling;
+nextSibling.style.backgroundColor = "yellow";
+*/
+
+// -- .previousElementSibling -- //
+/*
+const element = document.getElementById("orange");
+const prevSibling = element.previousElementSibling;
+prevSibling.style.backgroundColor = "orange";
+*/
+
+// -- .parentElement -- //
+/*
+const element = document.getElementById("apple");
+const parent = element.parentElement;
+parent.style.backgroundColor = "blue";
+*/
+
+// -- .children -- //
+const element = document.getElementById("vegetables");
+const children = element.children;
+
+
+children[1].style.backgroundColor = "yellow";
