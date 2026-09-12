c5d5d5f0b96a076c363aa0577aa7ff1c4c704291
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-09-13 00:28:38 +0600

    and today we end here

---
+++ image slider/index.js
@@ -0,0 +1,31 @@
+// IMAGE SLIDER
+
+const slides = document.querySelectorAll(".slides img");
+let slideIndex = 0;
+let intervalId = null;
+
+document.addEventListener("DOMContentLoaded", initializeSlider);
+
+function initializeSlider() {
+    if (slides.length > 0) {
+        slides[slideIndex].classList.add("displaySlide");
+        intervalId = setInterval(nextSlide, 5000);
+    }
+}
+
+function showSlide(index) {
+    slides.forEach(slide => {
+        slide.classList.remove("displaySlide");
+    });
+    slides[index].classList.add("displaySlide");
+}
+
+function prevSlide() {
+    slideIndex = (slideIndex - 1 + slides.length) % slides.length;
+    showSlide(slideIndex);
+}
+
+function nextSlide() {
+    slideIndex = (slideIndex + 1) % slides.length;
+    showSlide(slideIndex);
+}
